569bb59b74c131bb02a31ee334203367943f6174
Alecha89 <Alecha89@yandex.ru>
2021-03-22 22:06:39 +0300

    3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-       int res = 0;
-      for (int i = 7; i>0; --i) {
-       res = i;
-          System.out.println(res);
-    }
+//       int res = 0;
+//      for (int i = 7; i>0; --i) {
+//       res = i;
+//          System.out.println(res);
+//  }
+            int n0 = 1;
+            int n1 = 1;
+            int n2;
+            System.out.print(n0+" "+n1+" ");
+            for(int i = 3; i <= 11; i++){
+                n2=n0+n1;
+                System.out.print(n2+" ");
+                n0=n1;
+                n1=n2;
+            }
+            System.out.println();
 
 
 
