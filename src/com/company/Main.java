9bb6a057cad251bcd09fd90f4126842e0358f64a
Alecha89 <Alecha89@yandex.ru>
2021-03-22 21:51:30 +0300

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-//        int res = 0;
-//       for (int i = 7; i>0; --i) {
-//        res = i;
-//           System.out.println(res);
-//       }
+       int res = 0;
+      for (int i = 7; i>0; --i) {
+       res = i;
+          System.out.println(res);
+    }
 
 
 
