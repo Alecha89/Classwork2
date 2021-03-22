2f09cecf556f84d0c9b0de1d42a7d49a04154091
Alecha89 <Alecha89@yandex.ru>
2021-03-22 21:25:40 +0300

    commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        int res = 0;
+//       for (int i = 7; i>0; --i) {
+//        res = i;
+//           System.out.println(res);
+//       }
+
+
+
+    }
+}
